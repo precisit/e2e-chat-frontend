6534f7922efd5583b23c2972cd0cba60ef53fc28
godiagonal <same_johansson@hotmail.com>
2017-04-17 16:35:35 +0200

    Adjustments for tutorial

--- js/main.js
+++ js/main.js
@@ -6,16 +6,19 @@ var sendButton = document.getElementById('send');
 var messageInput = document.getElementById('message');
 var messageList = document.getElementById('messages');
 
-sendButton.addEventListener('click', createMessage);
-
+// Handlers for sending messages to server
+sendButton.addEventListener('click', sendMessage);
 messageInput.addEventListener('keydown', function(e) {
   // Enter key pressed
   if (e.keyCode == 13) {
-    createMessage();
+    sendMessage();
   }
 });
 
-function createMessage() {
+// Handler for receiving messages from server
+socket.on('newMessage', receiveMessage);
+
+function sendMessage() {
   // Do nothing if the input field is empty
   if (messageInput.value.length == 0) {
     return;
@@ -28,14 +31,17 @@ function createMessage() {
   messageInput.value = '';
 }
 
-socket.on('newMessage', function(message) {
+function receiveMessage(message) {
+  // Create a string containing the formatted message with time and message text
+  var formattedMessage = message.time + ' - ' + message.text;
+
   // Create a <li> element node
   var listItem = document.createElement('li');
-  // Create a text node containing the time and message text
-  var listItemText = document.createTextNode(message.time + ' - ' + message.text);
+  // Create a text node containing the formatted message
+  var listItemText = document.createTextNode(formattedMessage);
 
   // Append the text node to the <li> element node...
   listItem.appendChild(listItemText);
   // ... and append the <li> element node to the message list
   messageList.appendChild(listItem);
-});
+}
